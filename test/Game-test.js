51dfb623f2d4ff4f4bd04da00db7f47cb359a80f
Cole Vanacore <colevanacore@gmail.com>
2018-10-11 14:15:41 -0600

    Add more test files

--- test/Game-test.js
+++ test/Game-test.js
@@ -8,9 +8,30 @@ global.domUpdates = require('../domUpdates.js');
 chai.spy.on(global.domUpdates, [], () => true);
 
 
-
 describe ('Game', function() {
+  var game;
+  beforeEach(function() {
+    game = new Game();
+  });
+
   it('should return true', function() {
     expect(true).to.equal(true);
+  });
+
+  it('should have a winner', function() {
+    game = new Game('player 1');
+    expect(game.winner).to.equal('player 1');
+  });
+
+  it('should have 3 players', function() {
+    expect(game.players.length).to.equal(3);
+  });
+
+  it('should have 4 rounds', function() {
+    expect(game.rounds.length).to.equal(4);
+  });
+
+  it('should start round 1', function() {
+    
   })
 })
