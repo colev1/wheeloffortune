d4c2811a5ac646d8f266788ea6a6a66675be7c9d
Cole Vanacore <colevanacore@gmail.com>
2018-10-11 20:37:12 -0600

    Work on tests for puzzle

--- script.js
+++ script.js
@@ -8,19 +8,30 @@ $('.start-game-button').on('click', hideWelcomeMessage);
 $('.start-game-button').on('click', createNewGame);
 
 
-
-
-
-
-
 function createNewGame() {
   round = new Round();
   game = new Game(round);
   game.startGame();
-
   player1 = new Player();
   player2 = new Player();
   player3 = new Player();
+
+  puzzleBankIndex = Math.floor(Math.random() * 4);
+  selectPuzzleBank(puzzleBankIndex);
+
+}
+
+//generate puzzle bank for 
+function selectPuzzleBank(puzzleBankIndex) {
+  puzzle = new Puzzle();
+  puzzleBankArray = [ 'one_word_answers', 'two_word_answers', 'three_word_answers', 'four_word_answers'];
+  var puzzleBankArray = [ 'one_word_answers', 'two_word_answers', 'three_word_answers', 'four_word_answers' ];
+  // var puzzleBankArray = Object.keys(data);
+    console.log(data);
+    var puzzleBankofGame = puzzleBankArray[puzzleBankIndex];
+    var chosenBankArray = data.puzzles[puzzleBankofGame].puzzle_bank;
+    var selectedPuzzle = chosenBankArray[Math.floor(Math.random() * chosenBankArray.length)];
+    puzzle.generateNewPuzzle(selectedPuzzle);
 }
 
 
--- test/Puzzle-test.js
+++ test/Puzzle-test.js
@@ -2,6 +2,7 @@ const chai = require('chai');
 const expect = chai.expect;
 const Puzzle = require('../puzzle.js');
 const spies = require('chai-spies');
+const Data = require('../data-set.js');
 chai.use(spies);
 
 global.domUpdates = require('../domUpdates.js');
@@ -9,7 +10,29 @@ chai.spy.on(global.domUpdates, [], () => true);
 
 
 describe ('Puzzle', function() {
+  var puzzle;
+  beforeEach(function() {
+    puzzle = new Puzzle();
+  });
+
   it('should return true', function() {
     expect(true).to.equal(true);
   })
+
+  it('should take in a key index property and category property', function() {
+    puzzle = new Puzzle(2, 'Phrase');
+    expect(puzzle).to.eql({puzzleBankIndex: 2,category: 'Phrase'});
+  })
+
+  it('should be able to generate a new puzzle using the puzzle bank index', function() {
+  
+    puzzle.generateNewPuzzle();
+    // expect(puzzle.category).to.not.equal('');
+    expect(selectedPuzzle).to.exist();
+  });
+
+  it('should assign a category to puzzle once generate puzzle is invoked', function() {
+    puzzle.generateNewPuzzle();
+    expect(this.category).to.equal(selectedPuzzle.category);
+  });
 })
