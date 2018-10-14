06fbffee8dffeea45df2c31cffd115a0c0fcb006
Elizabeth Yeh <esnider1699@gmail.com>
2018-10-14 12:55:14 -0600

    wheel functions working, wheel tests added and added avatar images to css

--- test/Wheel-test.js
+++ test/Wheel-test.js
@@ -10,5 +10,17 @@ chai.spy.on(global.domUpdates, [], () => true);
 describe ('Wheel', function() {
   it('should return true', function() {
     expect(true).to.equal(true);
-  })
+  });
+
+  it('should have a default for the current values', function() {
+    expect(value.currentValue).to.equal([0]);
+  });
+
+  it('should be able to generate new values for each round', function() {
+    wheel.new Wheel(8);
+  });
+
+  it('should spin', function() {
+    wheel.spin()
+  });
 })
